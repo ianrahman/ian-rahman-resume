101116d0dfe859795e0f2aa13d9c4919629e3fb8
ianrahman <ian.alexander.rahman@gmail.com>
2016-12-04 21:11:55 -0500

    Added basic views and animations

--- IanRahman_Resume.playground/Contents.swift
+++ IanRahman_Resume.playground/Contents.swift
@@ -302,3 +302,41 @@ func makeMe() -> Ian {
 let ian = makeMe()
 
 ian.giveAutobiography()
+
+
+// Make things pretty
+import XCPlayground
+import PlaygroundSupport
+
+let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 375.0, height: 667.0))
+PlaygroundPage.current.liveView = containerView
+
+let circle = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 50.0, height: 50.0))
+circle.center = containerView.center
+circle.layer.cornerRadius = 25.0
+
+let startingColor = UIColor.blue
+circle.backgroundColor = startingColor
+
+containerView.addSubview(circle);
+
+let rectangle = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 50.0, height: 50.0))
+rectangle.center = containerView.center
+rectangle.layer.cornerRadius = 5.0
+
+rectangle.backgroundColor = .white
+
+containerView.addSubview(rectangle)
+
+UIView.animate(withDuration: 2.0, animations: { () -> Void in
+    let endingColor = UIColor.cyan
+    circle.backgroundColor = endingColor
+    
+    let scaleTransform = CGAffineTransform(scaleX: 5.0, y: 5.0)
+    
+    circle.transform = scaleTransform
+    
+    let rotationTransform = CGAffineTransform(rotationAngle: 3.14)
+    
+    rectangle.transform = rotationTransform
+})
